85bc15bf0bf91bf605126303f7bcb4f2baa3dc2d
Chris G <chris@saturn.earth>
2023-01-15 18:14:01 -0600

    This is the beginning of the header file for the parser.

---
+++ CplusPlusPort/Parser.h
@@ -0,0 +1,80 @@
+//**********************************************************************
+// file name: Parser.h
+//**********************************************************************
+
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// Buffer size defines.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+#define MAXLINE (80)    // Maximum length of input buffer.
+#define MAXPHO (1000)   // Size of phonem buffer.
+#define NUM_PHON (64)   // Number of phonems
+#define NUM_RULE (100)  // Number of rules allowed.
+
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+struct PhonemToCodeEntry
+{
+  char *phonemString;
+  uint8_t phonemByte;
+};
+
+class Parser
+{
+  public:
+
+  Parser(void);
+  ~Parser(void);
+
+  void STR_T_COD(char *PH_STR);
+  void PH_TO_COD(void);
+  void BLD_LIT_P(int RUL_INDX)
+  void SC_RT_CTX(int RT_INDX, VAR int RUL_INDX, bool *FOUND);
+  void SC_LF_CTX(int LEF_INDX, bool *FOUND);
+  bool CMP_REF_S;
+  void BLD_REF_S(int LEF_INDX, int *RT_INDX);
+  void FI_LF_PAR(int *LEF_INDX);
+  bool SCAN;
+  void RUL_SRCH(int BLK_OFF, int BLK_SIZ);
+
+  private:
+
+  //*****************************************
+  // Utility functions.
+  //*****************************************
+  bool IS_ALPHA(char PAR1);
+  bool IS_VWL(char PAR1);
+  bool IS_FR_VWL(char PAR1);
+  bool IS_CST(char PAR1);
+  bool IS_VO_CST(char PAR1);
+  void RT_PS_VWL(int *PAR1, bool *PAR2);
+  void LF_PS_VWL(int *PAR1, bool *PAR2);
+  bool RT_PS_CST(int *PAR1, bool *PAR2);
+  void LF_PS_CST(int *PAR1, bool *PAR2);
+
+  //*****************************************
+  // Attributes.
+  //*****************************************
+  char E_BUFFER[MAXLINE+1];
+  char P_BUFFER[MAXPHO+1];
+  char RUL_TBL[NUM_RULE+1][30];
+  PhonemToCodeEntry PHO_TBL[NUM_PHON+1];
+
+  char R_BUFFER[31];
+  char REF_STR[31];
+  char PH_STR[6];
+
+
+  int E_LEN;
+  int E_INDEX;
+  int P_INDEX;
+
+};
+
+#endif // _PARSER_H_
