3c12d9777d280ac8df8dc6d162dd140d5666819b
Chris G <chris@saturn.earth>
2023-01-19 21:40:47 -0600

    I think we have a nonworking baseline. I will now create development branches for future work.

--- CplusPlusPort/talker.cc
+++ CplusPlusPort/talker.cc
@@ -8,9 +8,25 @@
 
 #include "PhonemMaker.h"
 
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// Static storage for temporary information.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 static PhonemToCodeEntry phonemTable[128];
 static std::string rules[100];
-static char rule[100];
+static char  rule[100];
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+//*************************************************************************
+// This program provides the mainline code for a text-to-speech
+// synthesizer.  It accepts input from stdin so that a user can type a
+// line of text followed by a carriage return, or input can be taken from
+// a text file by using I/O redirection.  In either case, when a newline
+// occurrs, the text will be presented to the speech synthesis system.
+// I ported the original program, written in Pascal/MT+, to C++.  It will
+// be evident that I used an ancient compiler due to the short variable
+// names and short procedure and function names.  I also used upper case
+// text (we did that back then in the CP/M days).
+//*************************************************************************
 
 /**************************************************************************
 
@@ -147,17 +163,60 @@ static bool getSystemParameters(int& ruleCount,int& phonemCount)
 
 } // getSystemParameters
 
+//***********************************************************************
+// Mainline code.
+//***********************************************************************
 int main(int argc, char **argv)
 {
+   bool success; 
+   bool done;
    int ruleCount;
    int phonemCount;
+   char *statusPtr;
+   char englishBuffer[1000];
+   std::string englishText;
+   uint32_t phonemBufferLength;
+   uint8_t *phonemCodePtr;
    PhonemMaker *makerPtr;
 
-   // Now that information has been loaded, create the PhonemMaker object.
-   makerPtr = new PhonemMaker(rules,
-                              ruleCount,
-                              phonemTable,
-                              phonemCount);
+   // Retrieve the inputs to the PhonemMaker constructure.
+   success = getSystemParameters(ruleCount,phonemCount);
+
+   if (success)
+   {
+      // Instantiate our first object.
+      makerPtr = new PhonemMaker(rules,
+                                 ruleCount,
+                                 phonemTable,
+                                 phonemCount);
+
+      // Set up for loop entry.
+      done = false;
+
+      while (!done)
+      {
+         statusPtr = fgets(englishBuffer,100,stdin);
+
+         if (statusPtr != NULL)
+         {
+            // Nuke the newline!
+            englishBuffer[strlen(englishBuffer)-1] = '\0';
+
+            // Copy this in order to satify the interface.
+            englishText = englishBuffer;
+
+            // Generate the phonems.
+            makerPtr->acceptEnglishText(englishText,
+                                        phonemCodePtr,
+                                        phonemBufferLength);
+         } // if
+         else
+         {
+            // Bail out.
+            done = true;
+         } // else
+      } // while
+   } // if
 
    // Release resources.
    delete makerPtr;
